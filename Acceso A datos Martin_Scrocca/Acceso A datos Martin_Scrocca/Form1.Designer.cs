041629c030e80e02ebc853c967d924721ebda905
martinscrocca <53389578+martinscrocca@users.noreply.github.com>
2020-06-30 10:45:50 -0300

    .

--- Acceso A datos Martin_Scrocca/Acceso A datos Martin_Scrocca/Form1.Designer.cs
+++ Acceso A datos Martin_Scrocca/Acceso A datos Martin_Scrocca/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.btnListaMayores_25 = new System.Windows.Forms.Button();
             this.btnBuscarApell = new System.Windows.Forms.Button();
             this.txtApell = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnListaComp
@@ -71,11 +75,40 @@
             this.txtApell.Size = new System.Drawing.Size(100, 20);
             this.txtApell.TabIndex = 3;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(578, 365);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(188, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(188, 297);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txtApell);
             this.Controls.Add(this.btnBuscarApell);
             this.Controls.Add(this.btnListaMayores_25);
@@ -83,6 +116,7 @@
             this.Name = "Form1";
             this.Text = "Form Principal";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,6 +128,9 @@
         private System.Windows.Forms.Button btnListaMayores_25;
         private System.Windows.Forms.Button btnBuscarApell;
         private System.Windows.Forms.TextBox txtApell;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
